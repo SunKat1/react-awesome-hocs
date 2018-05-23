6e53127ee89c7df09829eda576adf96286133a34
Ivan Tyshchenko <tihon@ciat.net.ua>
2018-05-23 16:41:13 +0300

    some fixtures

--- src/fields/makeDebounced.js
+++ src/fields/makeDebounced.js
@@ -1,54 +1,53 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { debounce, omit } from "lodash";
+import { debounce, omit } from 'lodash';
 
-const makeDebounced = WrappedComponent => {
+const makeDebounced = (WrappedComponent) => {
   class Wrapper extends Component {
+    static propTypes = {
+      delay: PropTypes.number,
+      onChange: PropTypes.func.isRequired,
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        .isRequired,
+    };
+
+    static defaultProps = {
+      delay: 1500,
+    };
+
     constructor(props) {
       super(props);
 
       this.state = {
         value: null,
-        debouncedProps: omit(props, ["onChange", "value"])
+        debouncedProps: omit(props, ['onChange', 'value']),
       };
-
-      this._debounceChange = this._debounceChange.bind(this);
       this.sendExternalChange = debounce(props.onChange, props.delay);
     }
-    static propTypes = {
-      delay: PropTypes.number,
-      onChange: PropTypes.func.isRequired,
-      value: PropTypes.any.isRequired
-    };
 
-    static defaultProps = {
-      delay: 1500
-    };
-
-    static getDerivedStateFromProps = (
-      { delay, value, ...props },
-      prevState
-    ) => ({
+    static getDerivedStateFromProps = ({ delay, value, ...props }) => ({
       value,
-      debouncedProps: omit(props, ["onChange", "value"])
+      debouncedProps: omit(props, ['onChange', 'value']),
     });
 
-    _debounceChange(e, { value }) {
-      console.log("up", e, value, this.state);
-      this.setState({ value }, () => this.sendExternalChange(e, value));
+    componentDidUpdate(prevProps) {
+      if (prevProps.value !== this.props.value) {
+        this.sendExternalChange.cancel();
+      }
     }
 
     render() {
       const {
-        state: { value, debouncedProps }
+        state: { value, debouncedProps },
       } = this;
       return (
         <WrappedComponent
           {...{
             ...debouncedProps,
             value,
-            onChange: this._debounceChange
+            onChange: this.setState({ value }, () =>
+              this.sendExternalChange(e, value)),
           }}
         />
       );
